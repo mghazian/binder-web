2eaa85ad4f24cf94e9a08e619708f0d3fdbae922
Muhammad Ghazian <mghazian@github.com>
2025-09-29 14:13:36 +0700

    cleanup: Remove unused sql statement

--- src/data/sql.tsx
+++ src/data/sql.tsx
@@ -1,15 +1,13 @@
 import postgres from 'postgres';
 
 const getPostgreInstance = (() => {
-  let instance: postgres.Sql<{}> | null = null;
+  let instance: postgres.Sql<any> | null = null;
 
   return function () {
     if ( instance === null ) {
       // TODO: Move values to env or secrets
       instance = postgres('postgresql://localhost:35432/heybinder', { ssl: false, user: 'app', pass: 'app' });
     }
-
-    instance`select * from black`;
 
     return instance;
   }
