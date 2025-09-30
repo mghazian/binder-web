d12fcd15892231b0311f75fdcaa881775ad3166e
Muhammad Ghazian <mghazian@github.com>
2025-09-30 21:57:02 +0700

    feat: Allow SSL usage determine by environment variable

--- src/data/sql.tsx
+++ src/data/sql.tsx
@@ -1,12 +1,27 @@
 import postgres from 'postgres';
 
+const processUseSsl = (useSsl: string | undefined) => {
+  useSsl = useSsl?.toLowerCase();
+
+  switch ( useSsl ) {
+    case "true": return true;
+    case "false": return false;
+    case "require": return "require";
+    case "prefer": return "prefer";
+    case "allow": return "allow";
+    case "verify-full": return "verify-full";
+    default: return undefined;
+  }
+}
+
 const getPostgreInstance = (() => {
   let instance: postgres.Sql<any> | null = null;
 
   return function () {
     if ( instance === null ) {
-      const { POSTGRES_URL, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
-      instance = postgres(POSTGRES_URL!, { ssl: false, user: POSTGRES_USER, pass: POSTGRES_PASSWORD });
+      const { POSTGRES_URL, POSTGRES_USER, POSTGRES_PASSWORD, USE_SSL } = process.env;
+
+      instance = postgres(POSTGRES_URL!, { ssl: processUseSsl(USE_SSL), user: POSTGRES_USER, pass: POSTGRES_PASSWORD });
     }
 
     return instance;
